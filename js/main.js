17c589645b198ca1fe888569e6bd3225a976ec54
Admin <alenapachkovskay@gmail.com>
2026-02-10 17:53:40 +0700

    feat: begining

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
+
 let app = new Vue({
     el: '#app',
     data: {
-        product: "cards"
-    }
- })
- 
+        columns: [
+            {
+                columnId: 1,
+                columnTitle: "Запланировано",
+                maxCards: 3,
+                columnCards: [
+                    {
+                        id: 11,
+                        title: "Продукты",
+                        items: [
+                            { text: "сырр", done: false },
+                            { text: "молоко", done: false }
+                        ]
+                    }
+                ]
+            },
+            {
+                columnId: 2,
+                columnTitle: "В работе",
+                maxCards: 5,
+                columnCards: {},
+
+            },
+            {
+                columnId: 3,
+                columnTitle: "Выполнено",
+                columnCards: {},
+
+            }
+        ]
+    },
+});
